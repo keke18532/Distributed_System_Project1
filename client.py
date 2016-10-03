7095dfb5e0e6f1b4d425bd337fbcdfafe512e28f
keke18532 <keke18532@gmail.com>
2016-10-03 13:38:58 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,6 @@ from _thread import *
 
 filename=sys.argv[1]#get configuration filename from command line
 ID=sys.argv[2]#get ID for node
-#print(filename,ID)
 s=socket.socket()
 host=socket.gethostname()
 i=0,j=0
@@ -32,23 +31,38 @@ for i in range(0,len(line)):
 			s.send(bytes(info,'utf-8'))
 
 def clientthread():
-	clock=0
+	l=0#the lamport clock value
 	def local_event():
-		r=randrange(1,6)		
+		n=random.randrange(1,6)		
+		l=l+n
+		print('l '+n)
 	def send_message():
-		ra=randrange(0,length(nodelist))
+                ra=random.randrange(0,length(nodelist))
 		nodeport=nodelist[ra]
 		for i in range(0,len(line)):
 			if line[i].strip().split(' ')[1]==nodeport:
 				nodeID=line[i].strip().split(' ')[0]
 		s.connect(host,nodeport)
 		msg=ID+' '+l
-		s.send(bytes(info,'utf-8'))
+		s.send(bytes(msg,'utf-8'))
+                print('s '+nodeID+' '+l)
+        def receive_message():
+                msg=s.recv(1024)
+                if msg!=' ':
+                        start_new_thread(messagethread,(msg))
+                def messagethread(msg):
+                        sender=msg.split(' ')[0]
+                        clock=int(msg.split(' ')[1])
+                        l=l+clock
+                        printf('r '+sender+' '+clock+' '+l)
 
-
-
-
-
+        for j in range(0,100):
+                i=random.range(0,2)
+                if i==0:
+                        local_event()
+                else:
+                        send_message()
+        exit()
 
 
 
@@ -61,4 +75,3 @@ while True:
     buf=s.recv(1024)
     if(buf=='yes'):
 	start_new_thread(clientthread)
-
