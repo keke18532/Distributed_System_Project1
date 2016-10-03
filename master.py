d65273597ffb2903bb40c9f5c2ea570893812b9d
keke18532 <keke18532@gmail.com>
2016-10-03 23:26:41 +0300

    Update master.py

--- master.py
+++ master.py
@@ -1,13 +1,17 @@
 import socket
 import linecache
 
-s=socket.socket()
+
 host=socket.gethostname()
 
-line=linecache.getlines('config.txt')
-for i in range(0,len(line)):
-	s.connect(line[i].strip().split(' ')[1])
-	msg='yes'
-	s.send(bytes(msg, 'utf-8'))
-
+lines=linecache.getlines('config.txt')
+for i in range(0,len(lines)):
+	s=socket.socket()
+	if(s.connect_ex((host,int(lines[i].strip().split(' ')[1])))==0):
+		print('okay')
+		msg='yes'
+		s.send(bytes(msg, 'utf-8'))
+		print('sent')
+	s.close()
 	
+
